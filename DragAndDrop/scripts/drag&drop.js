2f87982527fdebcad9ce47b5b07d65f4d34dd05f
Andreea Miruna Sandru <pudelyna@yahoo.com>
2013-03-04 11:56:18 +0200

    comment on js file

--- DragAndDrop/scripts/drag&drop.js
+++ DragAndDrop/scripts/drag&drop.js
@@ -5,45 +5,65 @@ $(function() {
 	
 	//logging function for user console
 	var log = function(text){
+	
+		//JQuery selector to get console div with id 'consoleLog' in the context of the parent window
 		$('#consoleLog', window.parent.document).append($('<p>' + text + '</p>'));
+		
 		//scroll bottom in the console view
 		var height = $('#consoleLog', window.parent.document)[0].scrollHeight;
         $('#consoleLog', window.parent.document).scrollTop(height);
+		
 	};
 	
+	//JQuery tag name + attribute value selector
 	$('img[draggable="true"]').each(function (){
 	
-	//bind dragstart event to each draggable image in the source frame
+	   //manipulate dragstart event for each draggable image in the source frame
  		$( this ).bind('dragstart',function( event ){
+		
+			//the user can only MOVE the objects, but not copy them
  			event.originalEvent.dataTransfer.effectAllowed = 'move';
+			
+			//if dragg starts and there is no multiselection add just the current id to the draggableObjects array
  			if(draggableObjects.length == 0){
  				draggableObjects.push($(this).attr('id'));
  			}
-			//attache draggable ids to current event
+			
+			//attach draggable ids to current event
  			event.originalEvent.dataTransfer.setData('Text', JSON.stringify(draggableObjects));
  			log('Drag event initiated for elements with IDS:'+JSON.stringify(draggableObjects));
 			
  			//reset draggable ids 
  			draggableObjects.length = 0;
+			
         });
  		
-		//if CTRL is pressed then enable multi selection
+		//if CTRL is pressed then enable multi selection and marg objects in the GUI as selected
  		$( this ).bind('click', function( event ) {
+		
  		    if(event.ctrlKey){
  		    	draggableObjects.push($(this).attr('id'));
  		    	$(this).addClass('selected');
+				
  		    	log('CTRL pressed in multiple selection: '+JSON.stringify(draggableObjects));
+				
+				//prevent default behaviour for click event
  		    	event.preventDefault();
  		    }
+			
  		});
  	});
 	
 	//stop propagation of dragover event in order to manipulate the drop event
 	$('#dropTarget').bind('dragover', function( event ){
+	
+	    //target area will only accept a move for dragged objects
 		event.originalEvent.dataTransfer.dropEffect = 'move';
 		event.preventDefault();
+		
 	});
  	
+	//manipulate the drop event
 	$('#dropTarget').bind('drop', function( event ){
 	
 	    //get the list of draggable ids from the original event
